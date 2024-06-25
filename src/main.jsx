fada0c7bf5faa79141e846b18671f353a8ed02e4
deji <dejioyelakin@gmail.com>
2024-06-25 18:20:28 +0100

    removed a bug in the video chat implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-import VideoCall from './components/VideoCall.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import VideoCall from "./components/VideoCall.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App />,
   },
   {
     path: "/call",
-    element: <VideoCall/>
-  }
-])
+    element: <VideoCall />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
